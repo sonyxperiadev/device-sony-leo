d6763a0916dde3d7ac625de2bbfa8b5969ae31db
David Viteri <davidteri91@gmail.com>
2015-04-08 01:07:29 +0200

    leo: add init.usb.rc to device specific
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- aosp_d6603.mk
+++ aosp_d6603.mk
@@ -33,7 +33,8 @@ PRODUCT_COPY_FILES += \
     device/sony/leo/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     device/sony/leo/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
     device/sony/leo/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/leo/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+    device/sony/leo/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/leo/rootdir/init.shinano.usb.rc:root/init.shinano.usb.rc
 
 PRODUCT_COPY_FILES += \
     device/sony/leo/rootdir/system/etc/tfa98xx/coldboot.patch:/system/etc/tfa98xx/coldboot.patch \
